7fbd9db555600b145f223fa8675f10c49a5e5675
Lucca Delmano <31869148+ldelmano@users.noreply.github.com>
2023-10-18 18:12:19 -0300

    update home

--- app.config.ts
+++ app.config.ts
@@ -19,16 +19,21 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: "https://www.github.com/nuxt-themes/alpine", // our github repository
+        // possible value are : true | false
+        enabled: false,
+
+        // our github repository
+        repository: "https://www.github.com/nuxt-themes/alpine",
+
+        text: "anexo03",
       },
       navigation: true, // possible value are : true | false
       alignment: "center", // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: "Follow me on", // string that will be displayed in the footer (leave empty or delete to disable)
+      message: "", // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: "nuxtlabs",
-      instagram: "atinuxt",
+      twitter: "",
+      instagram: "projetoanexo03",
       linkedin: {
         icon: "uil:linkedin",
         label: "LinkedIn",
