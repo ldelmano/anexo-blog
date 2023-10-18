67a9a0bdac866b5218c0318f95dbd61be74e675d
Lucca Delmano <luccadelmano@gmail.com>
2023-10-18 18:02:13 -0300

    update logo

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,8 @@
 export default defineAppConfig({
   alpine: {
-    title: "Alpine",
-    description: "The minimalist blog theme",
+    title: "anexo03",
+    description:
+      "Inexplicavelmente, somos alvo de um amor incondicional, que nos atrai e nos envolve de maneira irrestível. Sendo assim, fomos anexados a um plano perfeito, onde Pai, Filho e Espírito Santo atuam de maneira harmoniosa e infalível",
     image: {
       src: "/social-card-preview.png",
       alt: "An image showcasing my project.",
@@ -11,8 +12,8 @@ export default defineAppConfig({
     header: {
       position: "right", // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: "/common.png", // path of the logo
-        pathDark: "/light.png", // path of the logo in dark mode, leave this empty if you want to use the same logo
+        path: "/logo.svg", // path of the logo
+        pathDark: "/logo-dark.svg", // path of the logo in dark mode, leave this empty if you want to use the same logo
         alt: "anexo03", // alt of the logo
       },
     },
